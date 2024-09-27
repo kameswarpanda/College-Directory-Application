23f582adb399ecfab39fe9066e1b4e3c994212ac
Kameswar Panda <kameswarpanda11@gmail.com>
2024-09-27 22:39:58 +0530

    Initial Commit

---
+++ college-directory-(Backend SpringBoot)/src/test/java/com/kameswar/college_directory/CollegeDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kameswar.college_directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
